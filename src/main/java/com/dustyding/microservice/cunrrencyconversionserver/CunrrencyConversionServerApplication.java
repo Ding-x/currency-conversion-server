a82979c0bb9beb4feb8ab4698cd0035319d93365
Ding-x <xiao_peng0202@hotmail.com>
2019-07-20 17:29:17 -0400

    init

---
+++ src/main/java/com/dustyding/microservice/cunrrencyconversionserver/CunrrencyConversionServerApplication.java
@@ -0,0 +1,13 @@
+package com.dustyding.microservice.cunrrencyconversionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunrrencyConversionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CunrrencyConversionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dustyding/microservice/cunrrencyconversionserver/CunrrencyConversionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustyding.microservice.cunrrencyconversionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CunrrencyConversionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
