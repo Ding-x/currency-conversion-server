7d00a2fcc8cbc7b94491a0c75f3691cb8352a64a
Ding-x <xiao_peng0202@hotmail.com>
2019-07-21 12:49:30 -0400

    feign

--- src/main/java/com/dustyding/microservice/cunrrencyconversionserver/CunrrencyConversionServerApplication.java
+++ src/main/java/com/dustyding/microservice/cunrrencyconversionserver/CunrrencyConversionServerApplication.java
@@ -2,8 +2,10 @@ package com.dustyding.microservice.cunrrencyconversionserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.dustyding.microservice.cunrrencyconversionserver")
 public class CunrrencyConversionServerApplication {
 
 	public static void main(String[] args) {
